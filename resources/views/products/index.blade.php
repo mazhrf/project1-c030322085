3481f9453597e9dda027bb70b292751136baa902
mazharf <muhammadazharfikri990@gmail.com>
2024-04-27 03:16:02 +0800

    third commit

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -34,7 +34,7 @@
                                     @forelse ($products as $product)
                                     <tr>
                                         <td class="text-center">
-                                            <img src="{{ asset(' /storage/products/'.$product->image ) }}" class="rounded" style="width:150 px">
+                                            <img src="{{ asset('/storage/products/'.$product->image ) }}" class="rounded" style="width: 150px;">
                                         </td>
                                         <td>{{ $product->title }}</td>
                                         <td>{{ "Rp " . number_format($product->price,2,',','.')}}</td>
